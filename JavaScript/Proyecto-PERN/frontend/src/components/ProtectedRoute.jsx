09eab26cc3ef74ac63410195009a9e2b30f22ce4
Yamil J. Yunes <yunesyamil@gmail.com>
2024-11-14 20:42:57 -0300

    Folder Adjust

--- JavaScript/Proyecto-PERN/frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-export const ProtectedRoute = ({redirecTo, isAllowed, children}) => {
-    if(!isAllowed)return <Navigate to={redirecTo} replace />;
-    
-    return children ? children : <Outlet />;
-}
--- JavaScript/Proyecto-PERN/frontend/src/components/ui/index.js
+++
@@ -1,6 +0,0 @@
-export {Button} from './Button';
-export {Input} from './input';
-export {Card} from './Card';
-export {Label} from './Label';
-export {Container} from './Container';
-export {Textarea} from './Texrarea';
